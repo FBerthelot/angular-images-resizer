28d36d7c7648f81b9e3b83ab16a8c45ce4fbbb41
FBerthelot <florent.berthelot35@gmail.com>
2014-11-19 11:29:17 +0100

    exclude one service from test coverage, beacause it cannot be tested for now

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -45,7 +45,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        '../src/*.js': ['coverage']
+        '../src/*.js': ['coverage'],
+        '!../src/resizeLocalPic.js': ['coverage']
     },
 
 

f26f23e0940f8df2017ef2e6f539871fc6c9d5a9
FBerthelot <florent.berthelot35@gmail.com>
2014-12-07 20:41:57 +0100

    first version of the githubpage done

--- components/mainScreen/mainScreen-ctrl.js
+++ components/mainScreen/mainScreen-ctrl.js
@@ -4,25 +4,52 @@
 'use strict';
 
 angular.module('mainScreen')
-    .controller('mainScreenCtrl', ['$scope', '$document', 'resizeService', function($scope, $document, resizeService) {
-        $document[0].title = 'Angular-images-resizer';
+    .controller('mainScreenCtrl', ['$scope', '$document', 'resizeService', 'readLocalPicService',
+        function($scope, $document, resizeService, readLocalPicService) {
+            $document[0].title = 'Angular-images-resizer';
 
-        /**
-         * Basic démonstration of the service
-         */
-        var basicImgResized = document.createElement('img');
-        $scope.basicResize = function() {
-            resizeService.startResize('ressources/aurora.jpg', function(err, image){
-                if(err) {
+            /**
+             * Basic démonstration of the service
+             */
+            var basicImgResized = document.createElement('img');
+            $scope.basicResize = function() {
+                resizeService.startResize('ressources/aurora.jpg', function(err, image){
+                    if(err) {
+                        console.error(err);
+                        return;
+                    }
+
+                    basicImgResized.src = image;
+                    document.getElementById('basicImageResized').appendChild(basicImgResized);
+                });
+            };
+
+            /**
+             * Demonstration from an input
+             */
+            var inputImgResized = document.createElement('img');
+            var inputImg = document.createElement('img');
+            var inputResize = function(file) {
+                $('.inputTitles').toggle();
+                readLocalPicService.readFileInput(file).then(function(img){
+                    inputImg.src = img;
+                    document.getElementById('inputImg').appendChild(inputImg);
+
+                    resizeService.startResize(img, function(err, image){
+                        if(err) {
+                            console.error(err);
+                            return;
+                        }
+                        inputImgResized.src = image;
+                        document.getElementById('inputImageResized').appendChild(inputImgResized);
+                    });
+                }, function(err){
                     console.error(err);
-                    return;
-                }
+                });
+            };
 
-                basicImgResized.src = image;
-                document.getElementById('basicImageResized').appendChild(basicImgResized);
+            //Bind the input file with the function on the top of this message
+            $('#fileInput').change(function() {
+                inputResize(this);
             });
-        };
-
-
-
     }]);

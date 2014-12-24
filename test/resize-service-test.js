1b7f1171a42144f4f9e194d397947bf52b772756
FBerthelot <florent.berthelot35@gmail.com>
2014-12-24 01:05:26 +0100

    added test of size resizeing

--- test/resize-service-test.js
+++ test/resize-service-test.js
@@ -156,15 +156,109 @@ describe('[images-resizer][resize-service]', function() {
 
             img.src = 'fixture/img.jpg';
         });
+        it('should return null when no image is specified', function() {
+            var data = service.resizeImageBySize();
+            expect(data).to.be.null;
+        });
     });
 
     describe('- resizeImage -', function() {
-        it('should return a base64 img with no error', function(done) {
+        it('should return nothing when there is missing image', function(done){
+            service.resizeImage(null, {} ,function(err, data) {
+                expect(err).to.be.not.null;
+                expect(data).to.be.null;
+                done();
+            });
+        });
+        it('should return nothing when there is missing options', function(done){
+            service.resizeImage('fixture/img.jpg', null ,function(err, data) {
+                expect(err).to.be.not.null;
+                expect(data).to.be.null;
+                done();
+            });
+        });
+
+        it('should return a base64 img with no error when no specific option is specified', function(done) {
 
             service.resizeImage('fixture/img.jpg', {} ,function(err, data) {
                 expect(err).to.be.null;
                 expect(data).to.be.not.null;
                 expect(data).to.contain('data:image/jpeg;base64');
+                done();
+            });
+
+            setTimeout(function() {
+                    rootScope.$digest();
+                    setTimeout(function() {
+                        rootScope.$digest();
+                    },500);
+                },500);
+        });
+
+        it('should return a base64 img according to the size given in octet', function(done) {
+
+            service.resizeImage('fixture/img.jpg', {size: 5000, sizeScale: 'o'} ,function(err, data) {
+                expect(data).to.be.not.null;
+                expect(data).to.contain('data:image/jpeg;base64');
+                //check the size of the returned image
+                expect(Math.round(data.length - 'data:image/jpeg;base64,'.length)*3/4).to.be.below(5000);
+                done();
+            });
+
+            setTimeout(function() {
+                    rootScope.$digest();
+                    setTimeout(function() {
+                        rootScope.$digest();
+                    },500);
+                },500);
+        });
+
+        it('should return a base64 img according to the size given in Ko', function(done) {
+            var sizeInKo = 5000 / 1024;
+
+            service.resizeImage('fixture/img.jpg', {size: sizeInKo, sizeScale: 'ko'} ,function(err, data) {
+                expect(data).to.be.not.null;
+                expect(data).to.contain('data:image/jpeg;base64');
+                //check the size of the returned image
+                expect(Math.round(data.length - 'data:image/jpeg;base64,'.length)*3/4).to.be.below(5000);
+                done();
+            });
+
+            setTimeout(function() {
+                    rootScope.$digest();
+                    setTimeout(function() {
+                        rootScope.$digest();
+                    },500);
+                },500);
+        });
+
+        it('should return a base64 img according to the size given in Mo', function(done) {
+            var sizeInMo = 5000 / (1024*1024);
+
+            service.resizeImage('fixture/img.jpg', {size: sizeInMo, sizeScale: 'mo'} ,function(err, data) {
+                expect(data).to.be.not.null;
+                expect(data).to.contain('data:image/jpeg;base64');
+                //check the size of the returned image
+                expect(Math.round(data.length - 'data:image/jpeg;base64,'.length)*3/4).to.be.below(5000);
+                done();
+            });
+
+            setTimeout(function() {
+                    rootScope.$digest();
+                    setTimeout(function() {
+                        rootScope.$digest();
+                    },500);
+                },500);
+        });
+
+        it('should return a base64 img according to the size given in Go', function(done) {
+            var sizeInGo = 5000 / (1024*1024*1024);
+
+            service.resizeImage('fixture/img.jpg', {size: sizeInGo, sizeScale: 'go'} ,function(err, data) {
+                expect(data).to.be.not.null;
+                expect(data).to.contain('data:image/jpeg;base64');
+                //check the size of the returned image
+                expect(Math.round(data.length - 'data:image/jpeg;base64,'.length)*3/4).to.be.below(5000);
                 done();
             });
 
